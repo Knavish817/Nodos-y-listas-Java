c22140ac124f93ae80daba4824cc94828cdfcf3e
Rolando MoralesQuintero <128012102+Knavish817@users.noreply.github.com>
2025-09-03 16:17:43 -0600

    Add doubly linked list implementation

--- listas/src/Uso/Main.java
+++ listas/src/Uso/Main.java
@@ -2,18 +2,24 @@ package lineales;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear la lista
+        // Lista simple
         Lista m = new Lista();
-
-        // Agregar elementos
         m.agrega(14);
         m.agrega(42);
         m.agrega(7);
         m.agrega(99);
-
-        // Imprimir lista
         System.out.println("Contenido de la lista:");
         System.out.println(m.imprimirLista());
+
+        // Lista doble
+        ListaDoble doble = new ListaDoble();
+        doble.insertarInicio(10);
+        doble.insertarFinal(20);
+        doble.insertaEn(1, 15);
+        System.out.println("Lista doble:");
+        System.out.println(doble.imprimir());
+        doble.invertir();
+        System.out.println("Lista doble invertida:");
+        System.out.println(doble.imprimir());
     }
 }
-
