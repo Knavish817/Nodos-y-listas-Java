55f8a29e678f3c046878d47f9ccb675e937fd078
Rolando MoralesQuintero <128012102+Knavish817@users.noreply.github.com>
2025-09-03 16:24:17 -0600

    Demostrar todas las operaciones de listas

--- listas/src/Uso/Main.java
+++ listas/src/Uso/Main.java
@@ -2,18 +2,38 @@ package lineales;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear la lista
-        Lista m = new Lista();
+        // Uso de nodos simples y dobles
+        Nodo nodoSimple = new Nodo(1);
+        NodoDoble nodoDoble = new NodoDoble(2);
+        System.out.println("Nodo simple: " + nodoSimple.getDato());
+        System.out.println("Nodo doble: " + nodoDoble.getDato());
 
-        // Agregar elementos
+        // Lista simple
+        Lista m = new Lista();
         m.agrega(14);
         m.agrega(42);
         m.agrega(7);
         m.agrega(99);
-
-        // Imprimir lista
-        System.out.println("Contenido de la lista:");
+        System.out.println("Contenido de la lista simple:");
         System.out.println(m.imprimirLista());
+
+        // Lista doble
+        ListaDoble doble = new ListaDoble();
+        System.out.println("¿La lista doble está vacía? " + doble.esVacia());
+        doble.insertarInicio(10);
+        doble.insertarFinal(30);
+        doble.insertaEn(1, 20);
+        System.out.println("Lista doble: " + doble.imprimir());
+        System.out.println("Buscar 20: " + doble.busca(20));
+        System.out.println("Elemento en índice 1: " + doble.obtener(1));
+        System.out.println("Elimina inicio: " + doble.eliminaInicio());
+        System.out.println("Elimina final: " + doble.eliminaFinal());
+        doble.insertarInicio(5);
+        doble.insertarFinal(40);
+        System.out.println("Elimina en índice 1: " + doble.eliminaEn(1));
+        System.out.println("Lista después de eliminaciones: " + doble.imprimir());
+        doble.invertir();
+        System.out.println("Lista doble invertida: " + doble.imprimir());
+        System.out.println("¿La lista doble está vacía? " + doble.esVacia());
     }
 }
-
